ddbf77f8ff9a63cf2ba4767459ca16b1b3d7140b
PonRajani <ragu0005@algonquinlive.com>
2022-03-31 13:25:08 -0400

    Branch Commit

--- app/src/main/java/com/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/finalproject/MainActivity.java
@@ -1,10 +1,48 @@
 package com.cst2335.finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.home_icon:
+                Intent intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.search_icon:
+                Intent intent1=new Intent(this,MainActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.help_icon:
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+                alertDialog.setTitle("Information For User")
+                        .setMessage("Please enter the country name and the date in the form of YYYY_MM_DD then hit search")
+                        .setNeutralButton("Okay",(click, arg)->{});
+                break;
+
+        }
+        return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
